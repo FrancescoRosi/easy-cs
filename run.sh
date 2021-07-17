fa3677ef33c7e37838d11e628f359b0d0376c8c9
Francesco <52544979+francescorosi27@users.noreply.github.com>
2021-07-17 18:37:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ -f "$HOME/.easycsinstalled" ]; then
+    echo Booting up code-server and localtunnel...
     code-server --bind-addr localhost:6969 > cs.log &
     lt --port 6969
 else
+    echo Looks like it\'s the first time you ran Easy Code Server! Running install script...
     bash install.sh
 fi
