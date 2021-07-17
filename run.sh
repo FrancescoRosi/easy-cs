4e4fa0dd6785ca4750fc16b4128bd0cbd259b3af
Francesco <52544979+francescorosi27@users.noreply.github.com>
2021-07-17 19:51:38 +0200

    i am still dumb lolllll

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 if [ -f "$HOME/.easycsinstalled" ]; then
     source ~/.profile
     echo Booting up code-server and localtunnel...
-    $CS/code-server --bind-addr localhost:6969 --password $PASSWORD > cs.log &
+    $CS/code-server --bind-addr localhost:6969 > cs.log &
     echo Your password is $PASSWORD
     node $LT/lt.js --port 6969
 else
