a3b0a2a23e1d20e02c195a2222a34afa58f803cc
Francesco <52544979+francescorosi27@users.noreply.github.com>
2021-07-17 18:36:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo Downloading code-server...
 curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | cut -d '"' -f4 | wget -qi -
 tar -xf code-server*.tar.gz
+rm code-server*.tar.gz
+echo Installing localtunnel
 npm i -g localtunnel
 touch $HOME/.easycsinstalled
 echo Easy Code Server has been installed successfully! You can now run this script again to start code-server.
