d723d67fb9da5897cce2932cdb5b28a8cc26c3f5
Francesco <52544979+francescorosi27@users.noreply.github.com>
2021-07-18 12:45:34 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ if [ -f "$HOME/.easycsinstalled" ]; then
     echo Booting up code-server and localtunnel...
     $CS/code-server --bind-addr localhost:6969 > cs.log &
     node $LT/lt.js --port 6969 &
-    echo "For security reasons, you need to manually find out your password. All you need to do is open a new terminal instance and type:"
+    echo "For security reasons (and since you didn't specify --showpassword), you need to manually find out your password. All you need to do is open a new terminal instance and type:"
     echo "  echo \$PASSWORD"
-    echo "To find out why this is recommended, visit [page not done yet]."
+    echo "If you're on Replit, you can do so by opening the Shell tab. To find out why I did this, visit https://github.com/francescorosi27/easy-cs/wiki/Why-isn't-the-password-outputted-to-the-console-by-default%3F."
 else
     echo Looks like it\'s the first time you ran Easy Code Server! Running install script...
     bash install.sh
