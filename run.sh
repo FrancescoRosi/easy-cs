b361a3f89cced21ee1d8fd6f917080dabc029566
Francesco <52544979+francescorosi27@users.noreply.github.com>
2021-07-18 13:05:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,6 +20,6 @@ if [ -f "$HOME/.easycsinstalled" ]; then
     fi
     node $LT/lt.js --port 6969
 else
-    echo "Looks like it\'s the first time you ran Easy Code Server! Running install script..."
+    echo "Looks like it's the first time you ran Easy Code Server! Running install script..."
     bash install.sh
 fi
