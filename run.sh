0bea3ac675ba8cd6f022719b12c132284bc1feb3
Francesco <52544979+francescorosi27@users.noreply.github.com>
2021-07-18 12:31:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ if [ -f "$HOME/.easycsinstalled" ]; then
     source ~/.profile
     echo Booting up code-server and localtunnel...
     $CS/code-server --bind-addr localhost:6969 > cs.log &
-    echo Your password is $PASSWORD
-    node $LT/lt.js --port 6969
+    node $LT/lt.js --port 6969 &
+    echo "For security reasons, you need to manually find out your password. All you need to do is open a new terminal instance and type:"
+    echo "  echo \$PASSWORD"
+    echo "To find out why this is recommended, visit [page not done yet]."
 else
     echo Looks like it\'s the first time you ran Easy Code Server! Running install script...
     bash install.sh
